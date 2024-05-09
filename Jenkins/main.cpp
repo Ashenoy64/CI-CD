260df39efc4988df58a160c01c2697933934a685
Meowmaster <ashenoy64@gmail.com>
2024-05-09 20:40:39 +0530

    Jenkins

---
+++ Jenkins/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+
+int sub(int a,int b)
+{
+    return a-b
+}
+
+
+int main(int argc, char **argv)
+{
+    cout<<"Example for function in C++"<<endl;
+
+    cout<<"Sum of 2 and 3" <<sum(2,3)<<endl;
+    cout<<"Sub of 2 and 3" <<sub(2,3)<<endl;
+
+    cout<<"End"<<endl;
+
+    return 0;
+}
